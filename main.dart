8af00c1e99bf265d9b43c6c13402ceb576326bd9
UsadJaydip <114413179+UsadJaydip@users.noreply.github.com>
2025-02-14 16:12:47 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -9,63 +9,103 @@ void main(){
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login Page'),
-        ),
+        // appBar: AppBar(
+        //   title: Text('Sign In'),
+        // ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Login',style: TextStyle(
-              fontSize: 35,
-              color: Colors.teal,
+            Text('Sign In',style: TextStyle(
+              fontSize: 40,
+              color: Colors.black,
               fontWeight: FontWeight.bold
             ),),
+            
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10,
+                bottom: 10,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Enter Your Password And Email',style: TextStyle(
+                   fontSize: 15,
 
-            Form(child: Column(
-              children: [
-                SizedBox(height:  30),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your Emails',
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (String value) {
+                  ), ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(
+                right: 35,
+                left: 35,
+              ),
+              child: Form(child: Column(
+                children: [
+                  SizedBox(height:  30,),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your Emails',
+                      prefixIcon: Icon(Icons.email,color: Colors.teal,),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    onChanged: (String value) {
+                      },
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please Enter Email' : null;
                     },
-                  validator: (value) {
-                    return value!.isEmpty ? 'Please Enter Email' : null;
-                  },
-                ),
-
-                SizedBox(height:  30),
-                TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your Password',
-                    prefixIcon: Icon(Icons.password),
-                    border: OutlineInputBorder(),
                   ),
-                  onChanged: (String value) {
-                  },
-                  validator: (value) {
-                    return value!.isEmpty ? 'Please Enter Password' : null;
-                  },
-                ),
 
-                SizedBox(height:  30),
-                MaterialButton(onPressed: (){},
-                  minWidth: double.infinity,
-                child: Text('Login'),
-                  color: Colors.teal,
-                  textColor: Colors.white,
-                ),
-              ],
-            ))
+                  SizedBox(height:  30),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your Password',
+                      prefixIcon: Icon(Icons.lock,color: Colors.teal,),
+                      suffixIcon: InkWell(
+                        onTap: (){
+
+                        },
+                          child: Icon(Icons.visibility_off,color: Colors.teal,)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    onChanged: (String value) {
+                    },
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please Enter Password' : null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                      child: Text('Forgot Password',style: TextStyle(fontWeight: FontWeight.w500 ),)),
+
+                  SizedBox(height:  30),
+                  MaterialButton(onPressed: () {},
+                    minWidth: 200,
+                  child: Text('Login'),
+                    color: Colors.teal,
+                    textColor: Colors.white,
+                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+
+                  ),
+                ],
+              )),
+            )
           ],
         ),
       ),
